1ac6884dcb5a66d7c8211e3b55467ac01e806a96
Yugandhar Dhore <143815412+Yug1020@users.noreply.github.com>
2026-09-26 12:11:39 +0530

    Time: 3 ms (10.61%), Space: 19.3 MB (77.89%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -9,34 +9,25 @@ class Solution:
     def minDiffInBST(self, root: TreeNode | None) -> int:
         def recMin(current, diff, lastNodes):
             lastNodes.append(current.val)
-            print("diff", diff)
-            print(id(diff))
             if diff[0] == 1:
-                print("why not working")
                 return diff[0]
-            else:
-                if current and current.left and diff[0] != 1:
-                    sub = float('inf')
-                    for i in lastNodes:
-                        temp = abs(i - current.left.val)
-                        sub = min(sub, temp)
-                    # sub = abs(current.val - current.left.val)
-                    print("diff btw", current.val ," and", current.left.val ,"is", sub)
-                    if sub < diff[0]:
-                        diff[0] = sub
-                    print("choosen", diff)
-                    recMin(current.left, diff, lastNodes)
-                if current and current.right and diff[0] != 1:
-                    sub = float('inf')
-                    for i in lastNodes:
-                        temp = abs(i - current.right.val)
-                        sub = min(sub, temp)                    
-                    # sub = abs(current.val - current.right.val)
-                    print("diff btw", current.val ," and", current.right.val ,"is", sub)
-                    if sub < diff[0]:
-                        diff[0] = sub
-                    print("choosen", diff)
-                    recMin(current.right, diff, lastNodes)
+                
+            if current and current.left and diff[0] != 1:
+                sub = float('inf')
+                for i in lastNodes:
+                    temp = abs(i - current.left.val)
+                    sub = min(sub, temp)
+                if sub < diff[0]:
+                    diff[0] = sub
+                recMin(current.left, diff, lastNodes)
+            if current and current.right and diff[0] != 1:
+                sub = float('inf')
+                for i in lastNodes:
+                    temp = abs(i - current.right.val)
+                    sub = min(sub, temp)                    
+                if sub < diff[0]:
+                    diff[0] = sub
+                recMin(current.right, diff, lastNodes)
 
             return diff[0]
 
